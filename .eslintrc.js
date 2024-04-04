8b87f350c182c77e53ec7ed7b46a49f08cbbba4c
Mohamed Idrissi <mhdhafdi2@gmail.com>
2024-04-04 03:36:47 -0400

    fix: align ESLint rules with Prettier to resolve formatting conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,41 +5,40 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', "import", "simple-import-sort"],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'simple-import-sort'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'airbnb-typescript/base'
+    'airbnb-typescript/base',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist/'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    "@typescript-eslint/lines-between-class-members": "off",
-    "complexity": ["warn", { "max": 10 }],
-    "complexity": ["error", { "max": 15 }],
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
+    '@typescript-eslint/lines-between-class-members': 'off',
+    complexity: ['warn', { max: 10 }],
+    complexity: ['error', { max: 15 }],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
       {
         overrides: {
           constructors: 'no-public',
         },
       },
     ],
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     'prettier/prettier': [
       'error',
       {
         endOfLine: 'lf',
       },
     ],
-    "indent": "off",
-    "@typescript-eslint/indent": "error"
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
   },
-
 };
