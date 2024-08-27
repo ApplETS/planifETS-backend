f0d935674f1e05ec70870663bbbb13ce522d99d5
Mohamed Hafdi Idrissi <mohamed.hafdi-idrissi.1@ens.etsmtl.ca>
2024-08-26 23:40:06 -0400

    logger instead of console.log

--- src/common/utils/pdf/parser/pdfParserUtil.ts
+++ src/common/utils/pdf/parser/pdfParserUtil.ts
@@ -1,6 +1,9 @@
+import { Logger } from '@nestjs/common';
 import PDFParser, { Output } from 'pdf2json';
 
 export class PdfParserUtil {
+  private static logger = new Logger(PdfParserUtil.name);
+
   public static async parsePdfBuffer<T>(
     pdfBuffer: Buffer,
     processData: (pdfData: Output) => T,
@@ -8,7 +11,7 @@ export class PdfParserUtil {
     const parser = new PDFParser();
 
     return new Promise((resolve, reject) => {
-      parser.on('pdfParser_dataError', (errData) => console.error(errData));
+      parser.on('pdfParser_dataError', (errData) => this.logger.error(errData));
       parser.on('pdfParser_dataReady', async (pdfData) => {
         try {
           const result = await processData(pdfData);
