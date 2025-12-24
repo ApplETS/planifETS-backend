0c38af859e61ec3cdc4a4e9d55b6322d8204931f
Mohamed Idrissi <mhdhafdi2@gmail.com>
2025-12-23 23:33:12 -0500

    attempt to fix ci

--- test/test-utils/jest-prisma/jest-global-setup.ts
+++ test/test-utils/jest-prisma/jest-global-setup.ts
@@ -1,5 +1,6 @@
 import { assertIsTestDatabase } from './assert-test-db';
 import { loadTestEnv } from './load-env';
+import { resetDatabase } from './reset-db';
 import { runCommand } from './run-command';
 
 export default async function globalSetup(): Promise<void> {
@@ -8,10 +9,11 @@ export default async function globalSetup(): Promise<void> {
   const databaseUrl = process.env.DATABASE_URL;
   if (!databaseUrl) throw new Error('DATABASE_URL is missing in globalSetup');
 
-  
+
   assertIsTestDatabase(databaseUrl);
 
   // Ensure schema is up-to-date for the test DB
   await runCommand('yarn', ['prisma:migrate-prod'], process.env);
   await runCommand('yarn', ['prisma:generate'], process.env);
+  await resetDatabase(databaseUrl);
 }
